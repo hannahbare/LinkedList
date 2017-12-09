81453a94c8896f990eeca163a0b36d87a7935835
Maddy Dannenberg <mmdberg@gmail.com>
2017-12-09 11:21:10 -0500

    Initial commit

---
+++ LL.js
@@ -0,0 +1,16 @@
+// Phase 1
+// 1. add event listener to button to add it to bookmarks section
+// 2. add event listener to "Read" button that creates/deletes class .Read
+// (boolean)
+// 3. add event listener to "remove" button to remove bookmark from page
+
+// Phase 2
+// 1. create error message if there is nothing in either the title or the URL field
+//  (Or statement?)
+// 2. Make responsive?!
+
+// Phase 3
+// 1. disable/enable "enter" button based on if there are contents in either field
+// 2. count total number of links
+// 3. count total number of read links
+// 4. count total number of unread links

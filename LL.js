09e94403a8fcb84d79f065f55290a92f7587d983
Maddy Dannenberg <mmdberg@gmail.com>
2017-12-11 18:12:43 -0700

    Add new card when enter button is clicked

--- LL.js
+++ LL.js
@@ -37,31 +37,25 @@ function readToggle (){
 }
 
 function deleteButton () {
-  deleteButton.classList.remove('bookmark')
+  deleteButton.classList.remove('bookmark');
+  removeChild
 }
 
 function addBookmark (){
   console.log('agg');
-  var section = document.querySelector('.article-list');
   var newElement = document.createElement('article');
-  // var node = document.createTextNode('This is new')
-  section.appendChild(newElement);}
-//   var titleInput = document.getElementById('title-input');
-//   var urlInput = document.getElementById('url-input');
-//   // websiteTitle.innerText = titleInput.value;
-//   editBookmark(titleInput, urlInput);
-// }
+  var section = document.querySelector('.article-list');
+  // var node = document.createTextNode('This is new');
+  section.appendChild(newElement);
+  console.log(newElement);
+  console.log(section);
+  newElement.innerHTML = 
+        `<h2 id='website-title'> ${titleInput.value} </h2>
+                <p id="website-link"> ${urlInput.value} </p>
+                <p class="read-delete-buttons">
+                  <button id="read-button" class="bottom-button">Read</button>
+                  <button id="delete-button" class="bottom-button">Delete</button>
+                </p>`;
+  newElement.setAttribute('class', 'bookmark')
+}
 
-// function editBookmark(titleInput, urlInput) {
-//     this.websiteTitle = titleInput;
-//     this.websiteLink = urlInput;
-//     this.addBookmark = function(){
-//       websiteTitle.innerText = this.websiteTitle;
-//       websiteLink.innerText = this.websiteLink;
-//     }
-//   }
-
-
-// }
-// var createBookmark = new newBookmark (Arguments)
-//(new-card)(appendChild), keep class 

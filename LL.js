689ac6d45af1b617244d8afee467cb0ca7d091a7
Maddy Dannenberg <mmdberg@gmail.com>
2017-12-13 18:09:11 -0700

    Add finishing touches

--- LL.js
+++ LL.js
@@ -11,46 +11,42 @@ var form = document.querySelector('.wrapper');
 var bookmarkCount = document.querySelector('.bookmarkCount');
 var readCount = document.querySelector('.readCount');
 
+enterButtonDisabled(true);
 
+titleInput.addEventListener('keyup', titlePresent);
+urlInput.addEventListener('keyup', URLpresent);
+
+function URLpresent (){
+  enterButtonDisabled(false);
+};
+
+function titlePresent () {
+  enterButtonDisabled(false);
+};
+
+function enterButtonDisabled (booleanvalue) {
+  enterButton.disabled = booleanvalue;
+};
 
 enterButton.addEventListener('click', errorMessage);
   function errorMessage() {
   var titleValue = titleInput.value;
   var urlValue = urlInput.value;
   if (urlValue == '' || titleValue == '') {
-    console.log('did this work?');
     alert('Error, please add a title and a url for your website.');
-    console.log('Hannah');
- } else {addBookmark()}
+ } else {
+  addBookmark();
+ }
 };
-
-
-titleInput.addEventListener('keyup', titlePresent);
-// enterButton.addEventListener('click', addBookmark);
-urlInput.addEventListener('keyup', URLpresent);
-
-enterButtonDisabled(true)
-
-function URLpresent (){
-  enterButtonDisabled(false);
-  // boolean, &&''
-}
-function titlePresent () {
-  enterButtonDisabled(false);
-}
-
-function enterButtonDisabled (booleanvalue) {
-  enterButton.disabled = booleanvalue;
-} 
 
 section.addEventListener('click', function(event){
  if (event.target.className.includes('read-button')) {
   var readButton = event.target;
   readButton.classList.toggle('read-button-read');
   readButton.parentNode.parentNode.classList.toggle('read-button-bookmark');
-  countReadBookmarks()
+  countReadBookmarks();
   }
-})
+});
 
 section.addEventListener('click', function(event){
  if (event.target.className.includes('delete-button')) {
@@ -58,32 +54,32 @@ section.addEventListener('click', function(event){
   var bookmarkList = document.querySelectorAll('.bookmark');
   console.log(bookmarkList.length)
   bookmarkCount.innerText = 'Number of Bookmarks: ' + bookmarkList.length;
+  countReadBookmarks();
   }
-}) 
+}); 
 
 function addBookmark (){
   var newElement = document.createElement('article');
   section.appendChild(newElement);
   newElement.innerHTML = 
-        `<h2 id='website-title'> ${titleInput.value} </h2>
-                <p id="website-link"><a href="http://${urlInput.value}"> ${urlInput.value} </a></p>
-                <p class="read-delete-buttons">
-                  <button class="bottom-button read-button">Read</button>
-                  <button class="bottom-button delete-button">Delete</button>
-                </p>`;
+    `<h2 id='website-title'> ${titleInput.value} </h2>
+      <p id="website-link"><a href="http://${urlInput.value}"> ${urlInput.value} </a></p>
+      <p class="read-delete-buttons">
+        <button class="bottom-button read-button">Read</button>
+        <button class="bottom-button delete-button">Delete</button>
+      </p>`;
   newElement.setAttribute('class', 'bookmark');
   countBookmarks();
   form.reset();
-}
+  enterButtonDisabled(true);
+};
 
 function countBookmarks(){
   var bookmarkList = document.querySelectorAll('.bookmark');
-  console.log(bookmarkList.length)
   bookmarkCount.innerText = 'Number of Bookmarks: ' + bookmarkList.length;
-}
+};
 
 function countReadBookmarks(){
-    var readList = document.querySelectorAll('.read-button-read');
-    console.log(readList.length);
-    readCount.innerText = 'Number of Bookmarks Read: ' + readList.length;
-  }
+  var readList = document.querySelectorAll('.read-button-read');
+  readCount.innerText = 'Number of Bookmarks Read: ' + readList.length;
+};
